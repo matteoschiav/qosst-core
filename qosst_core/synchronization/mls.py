4bee26524ff6990545f320ea25c8174786c361ae
Matteo Schiavon <matteoschiav@yahoo.it>
2025-04-18 18:31:33 +0200

    Add synchronization choice in the configuration

---
+++ qosst_core/synchronization/mls.py
@@ -0,0 +1,51 @@
+# qosst-core - Core module of the Quantum Open Software for Secure Transmissions.
+# Copyright (C) 2021-2025 Matteo Schiavon
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+Maximum Length Sequence for synchronization
+"""
+import numpy as np
+
+from qosst_core.synchronization.synchronization import SynchronizationSequence
+from scipy.signal import max_len_seq
+
+class MaximumLengthSequence(SynchronizationSequence):
+    """
+    Maximum Length Sequence class.
+    """
+
+    nbits: int  #: number of bits of the sequence
+
+    def __init__(self, nbits: int) -> None:
+        """
+        Args:
+            nbits (int): number of bits of the sequence
+        """
+
+        self.nbits = nbits
+    
+    def sequence(self) -> np.ndarray:
+        """
+        Generate the Maximum Length Sequence
+        """
+        
+        return max_len_seq(nbits=self.nbits)
+    
+    def __repr__(self) -> str:
+        return f"MaximumLengthSequence(nbits={self.nbits})"
+
+    def __str__(self) -> str:
+        return f"Maximum Length Sequence (nbits = {self.nbits})"
