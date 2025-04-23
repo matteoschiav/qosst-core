ae56a542025a2d0e7cdd8d8fe4d064b0c192d69e
Matteo Schiavon <matteoschiav@yahoo.it>
2025-04-23 10:24:25 +0200

    Fix synchronization submodule init

--- qosst_core/synchronization/__init__.py
+++ qosst_core/synchronization/__init__.py
@@ -1,5 +1,5 @@
 # qosst-core - Core module of the Quantum Open Software for Secure Transmissions.
-# Copyright (C) 2021-2024 Yoann Piétri
+# Copyright (C) 2021-2025 Matteo Schiavon
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,23 +15,25 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """
-This module contains the necessary codes to generate the synchronization sequence.
+This module contains the necessary codes for generating the syncrhonization
+sequence.
 
 Here are some examples of use
 
 .. code-block:: python
 
-    from qosst_core.synchronization import MaximumLengthSequence, ZadoffChuSequence
+    from qosst_core.synchronization import ZadoffChuSequence, MaximumLengthSequence
 
-    # Create a Zadoff-Chu sequence of root 5 and length 3990
+    # Create a Zadoff-Chu synchronization sequence with root 5 and length 3989
     zc = ZadoffChuSequence(root=5, length=3989)
     sequence = zc.sequence()
 
-    # Create a Maximum Length sequence with number of bits 16
+    # Create a Maximum Length Sequence with 16 bits
     mls = MaximumLengthSequence(nbits=16)
     sequence = mls.sequence()
+
 """
 from .synchronization import SynchronizationSequence
 
-from .mls import MaximumLengthSequence
 from .zc import ZadoffChuSequence
+from .mls import MaximumLengthSequence
